594f0fbe881e8f9953f286e9726165d32b853173
Hanz Aquino <hanz.aquino@fishermall.com.ph>
2026-04-28 17:58:55 +0800

    update

--- py/after_import_export.py
+++ py/after_import_export.py
@@ -737,32 +737,33 @@ if run_treasury:
     conn.commit()
 
 
-try:
+### RUN in MySQL workbench
+# try:
     
-    cursor.execute("SET SQL_SAFE_UPDATES = 0;")
-    sql5 = """
-        UPDATE t_m_s_service_invoices AS si
-        LEFT JOIN e_b_m_events_moa_information AS moa ON moa.id = si.tin
-        LEFT JOIN e_b_m_locations AS loc ON loc.id = moa.location
-        SET 
-            si.award_notice_no = moa.moa_id, 
-            si.loc_code = COALESCE(loc.location_code, ""),
-            si.`status` = CASE
-                WHEN moa.moa_id IS NULL THEN 0
-                ELSE 1
-            END
-        WHERE si.customer_type = 'event' 
-        AND si.tin IS NOT NULL;
-    """
-    cursor.execute(sql5)
-    cursor.execute("SET SQL_SAFE_UPDATES = 1;")
+#     cursor.execute("SET SQL_SAFE_UPDATES = 0;")
+#     sql5 = """
+#         UPDATE t_m_s_service_invoices AS si
+#         LEFT JOIN e_b_m_events_moa_information AS moa ON moa.id = si.tin
+#         LEFT JOIN e_b_m_locations AS loc ON loc.id = moa.location
+#         SET 
+#             si.award_notice_no = moa.moa_id, 
+#             si.loc_code = COALESCE(loc.location_code, ""),
+#             si.`status` = CASE
+#                 WHEN moa.moa_id IS NULL THEN 0
+#                 ELSE 1
+#             END
+#         WHERE si.customer_type = 'event' 
+#         AND si.tin IS NOT NULL;
+#     """
+#     cursor.execute(sql5)
+#     cursor.execute("SET SQL_SAFE_UPDATES = 1;")
     
-    conn.commit()
-    print("Update successful")
+#     conn.commit()
+#     print("Update successful")
 
-except Exception as e:
-    conn.rollback()
-    print("Error:", e)
+# except Exception as e:
+#     conn.rollback()
+#     print("Error:", e)
 
 
 
